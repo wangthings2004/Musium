fdb98f27fc6d09e2050e6257fa5527f51a363693
Thinh <lequangthinh882@gmail.com>
2024-10-11 17:34:17 +0700

    Musium

---
+++ app/src/main/java/com/jcxdc/musium/PlayerFragment.kt
@@ -0,0 +1,45 @@
+package com.jcxdc.musium
+
+import android.media.MediaPlayer
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.jcxdc.musium.databinding.FragmentPlayerBinding
+
+class PlayerFragment : Fragment() {
+    private lateinit var binding: FragmentPlayerBinding
+    private lateinit var mediaPlayer: MediaPlayer
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate layout using DataBindingUtil
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_player, container, false)
+
+        // Khởi tạo MediaPlayer với file MP3 trong res/raw
+        mediaPlayer = MediaPlayer.create(requireContext(), R.raw.jack)
+
+        // Sự kiện khi nhấn vào nút play/pause
+        binding.ivPlay.setOnClickListener {
+            if (!mediaPlayer.isPlaying) {
+                mediaPlayer.start()
+            } else {
+                mediaPlayer.pause()
+            }
+        }
+
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        if (this::mediaPlayer.isInitialized) {
+            mediaPlayer.release()
+        }
+    }
+}
